b2f1e5023a84c53e1052c780ee875e5ea21d7b47
Andy Papa <ajpotato214@protonmail.com>
2024-05-08 20:36:10 -0700

    Add CardRepository count funtion
    Modify CardRepository tests to create and delete dev database after each test
    
    Remove no longer needed dev terraform scripts

--- backend/src/__tests__/repository/CardRepository.test.js
+++ backend/src/__tests__/repository/CardRepository.test.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-undef */
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBClient,
+  CreateTableCommand,
+  DeleteTableCommand,
+  waitUntilTableExists,
+  waitUntilTableNotExists,
+  BatchWriteItemCommand,
+} from '@aws-sdk/client-dynamodb';
 import {
   DynamoDBDocumentClient,
-  ScanCommand,
-  DeleteCommand,
   GetCommand,
+  PutCommand,
 } from '@aws-sdk/lib-dynamodb';
 import CardRepository from '../../repository/CardRepository';
 import Config from '../../../../config.dev.json';
@@ -14,38 +20,57 @@ const REGION = Config.region;
 const ENDPOINT = Config.endpoint;
 
 describe('CardRepository', () => {
-  const testCardRepository = new CardRepository(TABLE_NAME, REGION, {
-    endpoint: ENDPOINT,
-  });
-
   const client = new DynamoDBClient({
     region: REGION,
     endpoint: ENDPOINT,
   });
   const docClient = DynamoDBDocumentClient.from(client);
 
+  const testCardRepository = new CardRepository(TABLE_NAME, REGION, {
+    endpoint: ENDPOINT,
+  });
+
+  beforeEach(async () => {
+    // create the table before every test
+    const command = new CreateTableCommand({
+      TableName: TABLE_NAME,
+      AttributeDefinitions: [
+        {
+          AttributeName: 'card_id',
+          AttributeType: 'S',
+        },
+      ],
+      KeySchema: [
+        {
+          AttributeName: 'card_id',
+          KeyType: 'HASH',
+        },
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 100,
+        WriteCapacityUnits: 100,
+      },
+    });
+
+    await client.send(command);
+
+    await waitUntilTableExists(
+      { client, maxWaitTime: 180 },
+      { TableName: 'mox_galleria_cards' },
+    );
+  });
+
   afterEach(async () => {
-    // clear the database
-    let command = new ScanCommand({
+    // delete table after every test
+    const command = new DeleteTableCommand({
       TableName: TABLE_NAME,
     });
 
-    const response = await docClient.send(command);
-
-    if (response.Count === 0) {
-      return;
-    }
-
-    Object.values(response.Items).forEach(async (item) => {
-      command = new DeleteCommand({
-        TableName: TABLE_NAME,
-        Key: {
-          card_id: item.card_id,
-        },
-      });
-
-      await docClient.send(command);
-    });
+    await client.send(command);
+    await waitUntilTableNotExists(
+      { client, maxWaitTime: 180 },
+      { TableName: 'mox_galleria_cards' },
+    );
   });
 
   test('returns the status of the active cards DynamoDB Table', async () => {
@@ -76,5 +101,82 @@ describe('CardRepository', () => {
     expect(response.Item.metadata).toStrictEqual({
       card_id: 'd50aee81-ac6b-42cf-84b2-c1cab286bcad',
     });
+  });
+
+  test('returns the count of cards in the table', async () => {
+    let count = await testCardRepository.count();
+    expect(count).toBe(0);
+
+    let command = new PutCommand({
+      TableName: TABLE_NAME,
+      Item: {
+        card_id: 'd50aee81-ac6b-42cf-84b2-c1cab286bcad',
+        provider: 'scryfall',
+        metadata: {},
+      },
+    });
+
+    await docClient.send(command);
+
+    count = await testCardRepository.count();
+    expect(count).toBe(1);
+
+    const input = {
+      RequestItems: {
+        mox_galleria_cards: [
+          {
+            PutRequest: {
+              Item: {
+                card_id: {
+                  S: '1f7a05c2-c3d7-4ae1-abd4-2ccd40ffc1f9',
+                },
+                provider: {
+                  S: 'scryfall',
+                },
+                metadata: {
+                  M: {},
+                },
+              },
+            },
+          },
+          {
+            PutRequest: {
+              Item: {
+                card_id: {
+                  S: 'cc16dbfc-eb1d-48bf-8cb1-79be885f22e4',
+                },
+                provider: {
+                  S: 'scryfall',
+                },
+                metadata: {
+                  M: {},
+                },
+              },
+            },
+          },
+          {
+            PutRequest: {
+              Item: {
+                card_id: {
+                  S: 'ca19ac96-0567-41a4-92c2-8214b0748492',
+                },
+                provider: {
+                  S: 'scryfall',
+                },
+                metadata: {
+                  M: {},
+                },
+              },
+            },
+          },
+        ],
+      },
+    };
+
+    command = new BatchWriteItemCommand(input);
+    await docClient.send(command);
+
+    count = await testCardRepository.count();
+    expect(count).toBe(4);
   });
 });
--- backend/src/repository/CardRepository.js
+++ backend/src/repository/CardRepository.js
@@ -1,6 +1,7 @@
 import {
   DynamoDBClient,
   DescribeTableCommand,
+  ScanCommand,
 } from '@aws-sdk/client-dynamodb';
 import { PutCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import IDynamoDBRepository from './IDynamoDBRepository';
@@ -35,5 +36,16 @@ export default class CardRepository extends IDynamoDBRepository {
     const response = await this.docddb.send(command);
 
     return response;
+  }
+
+  async count() {
+    const config = {
+      TableName: this.tableName,
+    };
+
+    const command = new ScanCommand(config);
+    const response = await this.ddb.send(command);
+
+    return response.Count;
   }
 }
